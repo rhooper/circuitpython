f1e7968486387a1dfdfcd7ae0fcb6428277ad168
Roy Hooper <rhooper@toybox.ca>
2018-06-18 22:44:30 -0400

    freeze against stripped down neopixel lib - still over by 412 bytes

--- ports/atmel-samd/boards/circuitplayground_express/mpconfigboard.mk
+++ ports/atmel-samd/boards/circuitplayground_express/mpconfigboard.mk
@@ -15,5 +15,5 @@ FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_BusDevice
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_CircuitPlayground
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_HID
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_LIS3DH
-# FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Thermistor
